f6c1798bac3ad27856825c5a0011fc63e3c131ee
Paul <impetus@yandex.ru>
2023-10-11 18:27:13 +0300

    Add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        int x = 100;
+
+
+
+
+        Scanner scanner = new Scanner();
 
     }
 }
