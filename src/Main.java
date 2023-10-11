7c3ae43a791070f3224988512a9c9f5bbab5fbb2
BIGkrossa <119872687+BIGkrossa@users.noreply.github.com>
2023-10-11 18:23:04 +0300

    Add qwe

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,12 @@ public class Main {
         System.out.println("Hello world!");
         int x = 100;
         System.out.println("Изи");
+        System.out.println("qwe");
+        System.out.println("qwe");
+        System.out.println("qwe");
+        System.out.println("qwe");
+        System.out.println("qwe");
+        System.out.println("qwe");
+        System.out.println("qwe");
     }
 }
