3e778ab1b0238ee94b7374e7273d22ad7ab2cd98
Paul <impetus@yandex.ru>
2023-10-11 18:29:06 +0300

    Add scanner2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-
-
-
         Scanner scanner = new Scanner();
         System.out.println("qwe");
         System.out.println("qwe");
